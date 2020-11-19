75c82b3776b1e500ac3ea6784b6f351306a28cd3
naelmessaoudene <naelmessaoudene@gmail.com>
2020-11-19 13:43:21 +0100

    add rotation between sun & self rotation

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform selfRotateTarget = null;
+    public int speed;
+    public int selfRotateSpeed;
+    void Update()
+    {
+        //turn around something
+        transform.RotateAround(target.transform.position, target.transform.up, speed*Time.deltaTime);
+        
+        //rotate on himself
+        transform.RotateAround(selfRotateTarget.transform.position, selfRotateTarget.transform.up, selfRotateSpeed*Time.deltaTime);
+
+    }
+}
