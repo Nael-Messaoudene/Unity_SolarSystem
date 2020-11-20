415328a1a8400aee0a6cfb1b5ea8536add741d86
naelmessaoudene <naelmessaoudene@gmail.com>
2020-11-20 11:39:57 +0100

    add final scene

---
+++ Assets/Scripts/Menu/FINAL/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void startExperience(){
+            SceneManager.LoadScene("SolarSystem");
+    }
+}
